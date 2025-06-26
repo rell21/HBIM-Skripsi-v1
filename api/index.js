ac848585a2ae5cfdf34232e6977e53c867198c77
rell21 <mfarrellw21@gmail.com>
2025-06-26 13:15:38 +0700

    Fix: Koreksi path untuk file statis di Express

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const auth = new AuthenticationClient(APS_CLIENT_ID, APS_CLIENT_SECRET);
 // Perubahan Kritis 3: Menggunakan path.join untuk path yang robust
 // __dirname di dalam Vercel menunjuk ke /var/task/api, jadi kita perlu naik dua level
 // untuk mencapai root proyek di mana folder 'www' berada.
-app.use(express.static(path.join(__dirname, '../../www')));
+app.use(express.static(path.join(__dirname, '../www')));
 
 app.get('/api/auth/token', async (req, res) => {
     try {
